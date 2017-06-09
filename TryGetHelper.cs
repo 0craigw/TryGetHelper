f74f6e73e1773680a0c0d5abfd217925d5089d2a
0craigw <Craig.Williams@lic.co.nz>
2017-06-09 16:00:18 +1200

    Initial

---
+++ TryGetHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LIC.Services.Reproduction.WebApi.IntegrationTests.Helpers
+{
+	/// <summary>
+	/// Implements TryGet method for any delegate
+	/// </summary>
+	public static class TryGetHelper
+	{
+		public static bool TryGet<T>(Func<T> function, out T result) 
+		{
+			try
+			{
+				result = function();
+			}
+			catch (Exception)
+			{
+				result = default(T);
+				return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ TryGetHelperTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using LIC.Services.Reproduction.Datastore.Db2;
+using LIC.Services.Reproduction.WebApi.IntegrationTests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LIC.Services.Reproduction.WebApi.IntegrationTests.UnitTests
+{
+	[TestClass]
+	public class TryGetHelperTests
+	{
+		[TestMethod]
+		public void ToString_ShouldBeTrue()
+		{
+			var x = 1;
+			string result;
+			var actual = TryGetHelper.TryGet(() => x.ToString(), out result);
+			Assert.IsTrue(actual);
+			Assert.AreEqual("1", result);
+		}
+
+		[TestMethod]
+		public void DivideByZero_ShouldBeFalse()
+		{
+			var x = 1;
+			int result;
+			var actual = TryGetHelper.TryGet(() => x / 0, out result);
+			Assert.IsFalse(actual);
+			Assert.AreEqual(0, result);
+		}
+
+
+		[TestMethod]
+		public void ComplexType_ShouldBeFalse()
+		{
+			FemaleReproductiveStatus result;
+			var actual = TryGetHelper.TryGet(() =>
+			{
+				throw new Exception();
+			}, out result);
+			Assert.IsFalse(actual);
+			Assert.AreEqual(null, result);
+		}
+	}
+}
